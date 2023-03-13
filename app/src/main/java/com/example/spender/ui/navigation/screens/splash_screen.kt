eb4a4fff1d93fac3366513e3c650caaa3a24d320
paveldolgov4 <dolgoff1272@gmail.com>
2023-03-13 14:59:47 +0300

    Добавил стили и Splash

--- app/src/main/java/com/example/spender/ui/navigation/screens/splash_screen.kt
+++ app/src/main/java/com/example/spender/ui/navigation/screens/splash_screen.kt
@@ -1,24 +1,83 @@
 package com.example.spender.ui.navigation.screens.first_screens
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.spender.R
 import com.example.spender.ui.navigation.BottomBar
 import com.example.spender.ui.navigation.nav_graphs.FirstNavGraph
 import com.example.spender.ui.navigation.screens.NavGraphs
 import com.example.spender.ui.navigation.screens.destinations.FirstScreenDestination
+import com.example.spender.ui.theme.BackgroundWhite
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.delay
 
+
+
+@Composable
+fun AnimatedSplashScreen(
+    navigator: DestinationsNavigator
+){
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
+    LaunchedEffect(key1 = true){
+        startAnimation = true
+        delay(4000)
+    }
+    Splash(alpha = alphaAnim.value)
+}
+@Composable
+fun Splash(alpha: Float){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundWhite),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier
+                    .alpha(alpha)
+                    .size(180.dp),
+                painter = painterResource(id = R.drawable.spender_icon),
+                contentDescription = "splash",
+                contentScale = ContentScale.Fit
+            )
+        }
+        Spacer(modifier = Modifier.size(60.dp))
+        Text(
+            "Spender",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @FirstNavGraph(start = true)
 @Destination
@@ -27,12 +86,6 @@ fun SplashScreen(
     navigator: DestinationsNavigator
 ) {
     // Splash screen layout
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Image(
-            painter = painterResource(id = R.drawable.sample_svg_bottom),
-            contentDescription = "splash"
-        )
-    }
     // Тут будет выбор запускаемого экрана в зависимости от того вошел юзер или нет
     val user_in = true // взятие user_state откуда-нибудь
     when (user_in) {
--- app/src/main/java/com/example/spender/ui/theme/Type.kt
+++ app/src/main/java/com/example/spender/ui/theme/Type.kt
@@ -1,17 +1,28 @@
 package com.example.spender.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.spender.R
 
 // Set of Material typography styles to start with
+val Roboto = FontFamily(
+    Font(R.font.roboto_medium)
+)
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        fontSize = 36.sp,
+        color = GreenMain,
+        letterSpacing = 10.sp
     )
     /* Other default text styles to override
     button = TextStyle(
